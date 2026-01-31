c85df06d23fa3bc801a5eadab373cad3542f9475
Sohaibkabeer <sohaibkabeer@gmail.com>
2026-01-31 17:53:15 +0500

    user  subscription schema created

---
+++ models/subscription.model.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+
+const subsciptionSChema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "subsciption Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 100,
+    },
+    price: {
+      type: String,
+      required: [true, "Subsciption price is required"],
+      minLength: [0, "Price must be greater than 0"],
+    },
+    currency: {
+      type: String,
+      enum: ["USD", "EUR", "GBP"],
+      default: "USD",
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+    },
+    category: {
+      type: String,
+      enum: [
+        "sports",
+        "news",
+        "entertainment",
+        "lifestyle",
+        "technology",
+        "finance",
+        "politics",
+        "other",
+      ],
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "cancelled", "expired"],
+      default: "active",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date must be in the past",
+      },
+    },
+    renewalDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+        message: "Start date must be in the past",
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+//Auto calculate renewal date of missing
+subsciptionSchema.pre("save", function (next) {
+  if (!this.renewalDate) {
+    const renewalPeriod = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
+
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.renewalDate.getData() + renewalPeriod[this.frequency],
+    );
+  }
+
+
+  //Auto-update the status if renewal date has passed
+  if (this.renewalDate < new Date()) {
+    this.status = 'expired';
+  }
+
+  next();
+});
+
+const Subsciption = mongoose.model("subsciption", subsciptionSChema);
+
+export default Subsciption;
---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSChema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "User Name is required"],
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\.\S+/, "Please fill a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: 6,
+  },
+},{timestamps:true});
+
+const User = mongoose.model('User',userSChema);
+
+export default User;
